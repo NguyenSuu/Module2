4e58f10af61ca89ff8968756b5f5d1d19b877527
SuuNguyen <suunguyen25121997@gmail.com>
2019-10-11 08:53:06 +0700

    Upload

--- JAVA/SystemTime/src/SystemTime.java
+++ JAVA/SystemTime/src/SystemTime.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Date;
 
 public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
 }
